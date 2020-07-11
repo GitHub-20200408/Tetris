5d695e19c6e7259c6f91784ec3588a2f353400ee
GitHub-20200408 <63351118+GitHub-20200408@users.noreply.github.com>
2020-07-11 17:16:04 +0800

    Create main.c
    主函数

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "game.h"
+int main()
+{
+    SetConsoleTitle("Tetris----Made by Big Data TSH");
+    gameInit();
+    return 0;
+}
